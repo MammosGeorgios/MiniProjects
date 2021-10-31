d0f9ca63e124481577f676b13e4b2b00d48690a5
MammosGeorgios <mammosgeorgios@gmail.com>
2021-10-31 19:25:05 +0200

    Added a core project and duplicated PersonalData class to the new project

---
+++ Core/src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main (String[] args){
+        System.out.println("This package will hold all the common classes that I will use in other internal projects. Also a test to see how we can combine different projects with maven");
+    }
+}
