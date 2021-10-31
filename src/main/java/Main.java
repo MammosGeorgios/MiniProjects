716a9038b76445895d58c13d7d32017fc0ed6246
MammosGeorgios <mammosgeorgios@gmail.com>
2021-10-31 19:40:05 +0200

    Cleaned up names and duplicate class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
+import common.PersonalData;
+
 public class Main {
 
     public static void main (String[] args){
         System.out.println("This is the main project and is used for quick tests when i can't be bothered to start a new project");
 
+        // We are calling this class from the subproject core
         PersonalData personalData =  PersonalData.getInstance();
         System.out.println(personalData);
     }
