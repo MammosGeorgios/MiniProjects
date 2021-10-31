bf93520b79f025655a0e0c2b4f6e3db3b785774e
MammosGeorgios <mammosgeorgios@gmail.com>
2021-10-31 19:34:48 +0200

    Changed core project to build to jar version and added a dependency from core project to the streamTests project!

--- StreamTests/src/main/java/Main.java
+++ StreamTests/src/main/java/Main.java
@@ -1,5 +1,9 @@
+import common.PersonalData;
+
 public class Main {
     public static void main (String[] args){
         System.out.println("StreamTests main method");
+        PersonalData personalData = PersonalData.getInstance();
+        System.out.println(personalData);
     }
 }
